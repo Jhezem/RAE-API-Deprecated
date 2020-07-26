3a97e372263131927f7122baf9087ac07c4a63f1
Walter Corrales <corraleswalter@live.com>
2020-07-25 22:26:51 -0600

    implementacion en express

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var scraper = require('./scrapper');
+
+app.get('/', async (req, res) => {
+    res.send('application loaded');
+});
+
+app.get('/existe/:word', async (req, res) => {
+    const { word } = req.params;
+    console.log(word);
+    res.send(await scraper.Existe(word));
+});
+
+app.listen(3000, () => {
+    console.log('Listen on port 3000');
+});
+
+module.exports = app;
